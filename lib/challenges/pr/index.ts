82af5fc2ec5178e9bb5464c70ea03781dcce6c38
venkat <venkat>
2026-04-17 20:15:34 +0200

    feat: add index file for challenges PR module

--- lib/challenges/pr/index.ts
+++ lib/challenges/pr/index.ts
@@ -18,6 +18,24 @@ import c007 from './ENG-PR-007';
 import c008 from './ENG-PR-008';
 import c009 from './ENG-PR-009';
 import c010 from './ENG-PR-010';
+import c011 from './ENG-PR-011';
+import c012 from './ENG-PR-012';
+import c013 from './ENG-PR-013';
+import c014 from './ENG-PR-014';
+import c015 from './ENG-PR-015';
+import c016 from './ENG-PR-016';
+import c017 from './ENG-PR-017';
+import c018 from './ENG-PR-018';
+import c019 from './ENG-PR-019';
+import c020 from './ENG-PR-020';
+import c021 from './ENG-PR-021';
+import c022 from './ENG-PR-022';
+import c023 from './ENG-PR-023';
+import c024 from './ENG-PR-024';
+import c025 from './ENG-PR-025';
+import c026 from './ENG-PR-026';
+import c027 from './ENG-PR-027';
+import c028 from './ENG-PR-028';
 
 export const prChallenges = [
   c001,
@@ -30,4 +48,22 @@ export const prChallenges = [
   c008,
   c009,
   c010,
+  c011,
+  c012,
+  c013,
+  c014,
+  c015,
+  c016,
+  c017,
+  c018,
+  c019,
+  c020,
+  c021,
+  c022,
+  c023,
+  c024,
+  c025,
+  c026,
+  c027,
+  c028,
 ];
